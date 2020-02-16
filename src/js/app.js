440c730252799605256633acb5e60c98b5cb7a80
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2020-02-16 19:22:13 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+const game = new Game();
+game.start();
+
+import class Game, class GameSavingData, {readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
---
+++ src/js/game.js
@@ -0,0 +1,24 @@
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+import class Character from './domain.js';
+
+export default class Game {
+  start() {
+  	console.log(game started);
+  }
+};
+
+export {GameSavingData, readGameSaving, writeGameSaving};
